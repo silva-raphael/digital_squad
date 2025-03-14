95f57e108a3776d3820998051cc91591b955155e
rlvv <rlvv@gft.com>
2025-03-14 17:41:01 -0300

    Added default logger

---
+++ app/logger.py
@@ -0,0 +1,41 @@
+import sys
+from datetime import datetime
+
+from loguru import logger as _logger
+
+# internal packages
+from config import PROJECT_ROOT
+
+_print_level = "INFO"
+
+def define_log_level(print_level="INFO", logfile_level="DEBUG", name: str = None):
+    """Adjust log level to level above"""
+    global _print_level
+    _print_level = print_level
+
+    current_date = datetime.now()
+    formatted_date = current_date.strftime("%Y/%m/%d%H%M%S")
+    log_format = "<green>{time:YYYY-MM-DD HH:mm:ss.SSS zz}</green> | <level>{level: <8}</level> | <yellow>Line {line: >4} ({file}):</yellow> <b>{message}</b>"
+    log_name = (
+        f"{name}_{formatted_date}" if name else formatted_date
+    )   # possibility to name the log with a specific name
+
+    _logger.remove()
+    _logger.add(sys.stderr, level=print_level, format=log_format)
+    _logger.add(PROJECT_ROOT / f"logs/{log_name}.log", level=logfile_level)
+    
+    return _logger
+
+logger = define_log_level()
+
+if __name__ == "__main__":
+    logger.info("Info message")
+    logger.debug("Debug message")
+    logger.warning("Warning message")
+    logger.error("Error message")
+    logger.critical("Critical message")
+
+    try:
+        raise ValueError("Test error")
+    except Exception as e:
+        logger.exception(f"Exception message: {e}")
