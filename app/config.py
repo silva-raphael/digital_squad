bdd82a2e72c6ad4ae647348f44f9adb1949cbc86
rlvv <rlvv@gft.com>
2025-03-14 17:41:34 -0300

    Added config file

---
+++ app/config.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+def get_project_root() -> Path:
+    """Get the project root directory"""
+    return Path(__file__).resolve().parent.parent
+
+PROJECT_ROOT = get_project_root()
