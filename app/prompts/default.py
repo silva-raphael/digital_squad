f90be682f099d557d654741dd8e94f19d62862d3
rlvv <rlvv@gft.com>
2025-03-18 15:10:30 -0300

    new_step prompt tuned for react agent

--- app/prompts/default.py
+++ app/prompts/default.py
@@ -1,3 +1,18 @@
 SYSTEM_INSTRUCTIONS = "You are a helpful assistant called NOVA that must help the user"
 
-NEXT_STEP = "Select the appropriate tool based on your needs or TERMINATE."
+NEXT_STEP = """Based on the request you should reflect on what you have to do. Output a clear line of thought in the following format:
+
+{
+'thought': <your thoughts and reflection>
+'action' : {
+    'tool_name': <selected_tool>
+    'reason': <reason why you chose this specific tool>
+    'tool_input': <dictionary following the tool definition>
+}
+}
+
+Avaiable tools are:
+
+- multiply_two_numbers: {a: float, b: float}
+
+"""
