49d6df82568b2233510971dceda8aeb18652af71
rlvv <rlvv@gft.com>
2025-03-20 10:28:00 -0300

    Created a user interaction tool

---
+++ app/tools/askuser.py
@@ -0,0 +1,15 @@
+from app.tools.base import Tool
+
+@Tool.as_tool
+def ask_user(question: str) -> str:
+    """Ask the user a question
+    
+    Args:
+        question: Question to the user to answer
+    
+        Returns:
+            User response
+    """
+    
+    answer = input(question)
+    return answer
